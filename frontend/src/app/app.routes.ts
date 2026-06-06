87a628598d1d0d284d49377d664d6ef138a13dd3
EmilioR <emiliosoad@gmail.com>
2026-06-05 22:10:56 -0300

    refactor: se crean componentes compartidos y rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+            { path: 'home', component: Home },
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+        ]
+    }
+];
