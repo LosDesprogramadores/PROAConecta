2d4389f48e0ff9c68128de497c9175656380e3bd
Marcelo Portillo <mportillo79@gmail.com>
2026-06-12 03:17:17 +0000

    feat: Se agrega clases al modelo Usuario

--- backend/proa/proa/settings.py
+++ backend/proa/proa/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+AUTH_USER_MODEL = 'usuario.Usuario'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -106,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
