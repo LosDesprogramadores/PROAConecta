6f43bdf1b2e2d3f08c74d595e4c6f929f977ebbb
AlandMarini <aland.marini@gmail.com>
2026-09-05 00:25:38 -0300

    fix(academico): Agregar restricción de eliminación de Materias

--- backend/proa/academico/views.py
+++ backend/proa/academico/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -20,13 +20,42 @@ class MateriaViewSet(viewsets.ModelViewSet):
         queryset = super().get_queryset()
         anio = self.request.query_params.get('anio')
         curso = self.request.query_params.get('curso')
+        profesor_id = self.request.query_params.get('profesor')
 
         if anio:
             queryset = queryset.filter(anio=anio)
         if curso:
             queryset = queryset.filter(curso__icontains=curso)
+        if profesor_id:
+            queryset = queryset.filter(profesor_id=profesor_id)
 
         return queryset
+
+    #Impedir eliminación de materias si la materia tiene un profesor asignado y/o al menos un estudiante.
+    def destroy(self, request, *args, **kwargs):
+
+        materia = self.get_object()
+        motivos = []
+
+        if materia.profesor is not None:
+            motivos.append("Tiene un profesor asignado")
+
+        total_inscriptos = materia.inscripciones.count()
+        if total_inscriptos > 0:
+            motivos.append(f"Tiene {total_inscriptos} estudiante(s) inscripto(s).")
+
+        if motivos:
+            return Response(
+                {
+                    "error": "No se puede eliminar la materia.",
+                    "motivos": motivos,
+                    "sugerencia": "Si elimina la materia, se eliminará toda información relacionada a la misma (Notas, Estudiantes)."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return super().destroy(request, *args, **kwargs)
+
 
     @action(detail=True, methods=['get'], url_path='estudiantes-disponibles')
     def estudiantes_disponibles(self, request, pk=None):
